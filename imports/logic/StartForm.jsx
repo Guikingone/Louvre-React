7f813d5da5c208e665c63369cf99d61eb77daeaf
Guikingone <guillaume.loulier@hotmail.fr>
2017-03-25 19:26:27 +0100

    [+](Logic)[!M] || Logic|Progress

--- imports/logic/StartForm.jsx
+++ imports/logic/StartForm.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import { Session } from 'meteor/session';
 
@@ -7,34 +6,32 @@ import Navbar from "../ui/Navbar";
 import Footer from "../ui/Footer";
 
 export default class StartForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            errors: ''
+        };
+    }
     handleSubmit(event) {
         event.preventDefault();
 
-        const lastname = ReactDOM.findDOMNode(this.refs.lastnameInput).value.trim();
-        const firstname = ReactDOM.findDOMNode(this.refs.firstnameInput).value.trim();
-        const email = ReactDOM.findDOMNode(this.refs.emailInput).value.trim();
-        const date = ReactDOM.findDOMNode(this.refs.dateInput).value.trim();
-        const reduction = ReactDOM.findDOMNode(this.refs.reductionInput).value.trim();
+    }
+    startOrder() {
+        // Start the order
+        Session.set({
 
-        if (lastname && firstname && email && date) {
-            event.target.lastname.value = '';
-            // Store the entire form into the session.
-            Session.set({
-                "Order" : {
-                    "lastname": lastname,
-                    "firstname": firstname,
-                    "email": email,
-                    "date": date,
-                    "reduction": reduction,
-                }
-            });
-        }
+        });
     }
     render() {
         return (
             <div>
                 <Navbar/>
-                    <form onSubmit={this.handleSubmit.bind(this)}>
+                {
+                    this.state.errors
+                    ? <p>{this.state.errors}</p>
+                    : undefined
+                }
+                <form onSubmit={this.handleSubmit.bind(this)}>
                         <label>
                             Nom :
                             <input type="text" name="lastname" ref="lastnameInput" />
@@ -56,7 +53,7 @@ export default class StartForm extends Component {
                             <input type="text" name="reduction" ref="reductionInput" />
                         </label>
                     </form>
-                    <button type="submit"><Link to="/tickets">Commencer la commande</Link></button>
+                    <button onClick={this.startOrder}><Link to="/tickets">Commencer la commande</Link></button>
                 <Footer/>
             </div>
         )
